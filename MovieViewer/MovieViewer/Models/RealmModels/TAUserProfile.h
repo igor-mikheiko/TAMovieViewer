ed51460e8b96ed24bab33ea6196e4624ae5a32e7
Alex Rudyak <al.rudyak@gmail.com>
2015-07-24 16:18:35 +0300

    user profile for device account

---
+++ MovieViewer/MovieViewer/Models/RealmModels/TAUserProfile.h
@@ -0,0 +1,15 @@
+//
+//  TAUserProfile.h
+//  MovieViewer
+//
+//  Created by Alex Rudyak on 7/24/15.
+//  Copyright (c) 2015 *instinctools. All rights reserved.
+//
+
+#import "RLMObject.h"
+
+@interface TAUserProfile : RLMObject
+
+@property NSString *username;
+
+@end
---
+++ MovieViewer/MovieViewer/Models/RealmModels/TAUserProfile.m
@@ -0,0 +1,13 @@
+//
+//  TAUserProfile.m
+//  MovieViewer
+//
+//  Created by Alex Rudyak on 7/24/15.
+//  Copyright (c) 2015 *instinctools. All rights reserved.
+//
+
+#import "TAUserProfile.h"
+
+@implementation TAUserProfile
+
+@end
