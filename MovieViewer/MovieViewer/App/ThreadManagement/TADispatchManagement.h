82c2f301b3d274006854f8ef22991829bc2fa7ee
Uladzimir Predka <uladzimir.predka@instinctools.ru>
2015-07-24 13:01:59 +0300

    renamed methods

--- MovieViewer/MovieViewer/App/ThreadManagement/TADispatchManagement.h
+++ MovieViewer/MovieViewer/App/ThreadManagement/TADispatchManagement.h
@@ -5,20 +5,20 @@
 
 #import <Foundation/Foundation.h>
 
-extern void ad_dispatch_async(dispatch_block_t block);
+extern void ta_dispatch_async(dispatch_block_t block);
 
-extern void ad_dispatch_safe_queue(dispatch_block_t block);
+extern void ta_dispatch_safe_queue(dispatch_block_t block);
 
-extern void ad_dispatch_main_queue(dispatch_block_t block);
+extern void ta_dispatch_main_queue(dispatch_block_t block);
 
-extern void ad_dispatch_on_queue(dispatch_queue_t queue, dispatch_block_t block);
+extern void ta_dispatch_on_queue(dispatch_queue_t queue, dispatch_block_t block);
 
-extern void ad_dispatch_after_on_queue(float seconds, dispatch_queue_t queue, dispatch_block_t block);
+extern void ta_dispatch_after_on_queue(float seconds, dispatch_queue_t queue, dispatch_block_t block);
 
-extern void ad_dispatch_after(float seconds, dispatch_block_t block);
+extern void ta_dispatch_after(float seconds, dispatch_block_t block);
 
 
-@interface ADDispatchPool : NSObject
+@interface TADispatchPool : NSObject
 
 - (instancetype)initWithQueueCount:(NSInteger)count;
 - (void)scheduleTask:(void (^)(void))task;
--- MovieViewer/MovieViewer/App/ThreadManagement/TADispatchManagement.m
+++ MovieViewer/MovieViewer/App/ThreadManagement/TADispatchManagement.m
@@ -5,35 +5,35 @@
 
 #import "TADispatchManagement.h"
 
-void ad_dispatch_async(dispatch_block_t block) {
-    ad_dispatch_on_queue(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block);
+void ta_dispatch_async(dispatch_block_t block) {
+    ta_dispatch_on_queue(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block);
 }
 
-void ad_dispatch_safe_queue(dispatch_block_t block) {
+void ta_dispatch_safe_queue(dispatch_block_t block) {
     if ([NSThread isMainThread]) {
         block();
     } else {
-        ad_dispatch_on_queue(dispatch_get_main_queue(), block);
+        ta_dispatch_on_queue(dispatch_get_main_queue(), block);
     }
 }
 
-void ad_dispatch_main_queue(dispatch_block_t block) {
-    ad_dispatch_on_queue(dispatch_get_main_queue(), block);
+void ta_dispatch_main_queue(dispatch_block_t block) {
+    ta_dispatch_on_queue(dispatch_get_main_queue(), block);
 }
 
-void ad_dispatch_on_queue(dispatch_queue_t queue, dispatch_block_t block) {
+void ta_dispatch_on_queue(dispatch_queue_t queue, dispatch_block_t block) {
     dispatch_async(queue, block);
 }
 
-void ad_dispatch_after_on_queue(float seconds, dispatch_queue_t queue, dispatch_block_t block) {
+void ta_dispatch_after_on_queue(float seconds, dispatch_queue_t queue, dispatch_block_t block) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, seconds * NSEC_PER_SEC), queue, block);
 }
 
-void ad_dispatch_after(float seconds, dispatch_block_t block) {
-    ad_dispatch_after_on_queue(seconds, dispatch_get_main_queue(), block);
+void ta_dispatch_after(float seconds, dispatch_block_t block) {
+    ta_dispatch_after_on_queue(seconds, dispatch_get_main_queue(), block);
 }
 
-@implementation ADDispatchPool {
+@implementation TADispatchPool {
     NSArray *targetQueues;
     NSInteger lastIdx;
 
